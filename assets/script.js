3e09137a90ca59fda6c830e9b23e85ee47c3fb36
Jens Broekens <jens.broekens@protonmail.com>
2025-05-28 01:41:35 +0200

    script.js: simplify lightbox accessibility
    Even if not technically correct, I think this is a better way
    of doing it. The images are first and foremost that: images.
    
    Not much value in wrapping the triggers in `<kbd>` and I don't
    expect the `display: contents` bugs to get fixed anytime soon.

--- assets/script.js
+++ assets/script.js
@@ -12,26 +12,16 @@ lbx.addEventListener("keyup", (e) => e.key === "Escape" && lbx.close());
 document.body.appendChild(lbx);
 
 document.querySelectorAll("main img:not(a img)").forEach((img) => {
-	/*
-	 * TODO:
-	 * Replace with `<button>` and remove `role` and `tabIndex`
-	 * Blocked-by: https://caniuse.com/css-display-contents (note 2)
-	 */
-	const btn = document.createElement("div");
-	btn.role = "button";
-	btn.tabIndex = 0;
-	btn.classList.add("lbox-btn");
-	btn.innerHTML = '<span class="sr-only">Press <kbd>Enter</kbd> or click on image to preview</span>';
-	btn.addEventListener("click", (e) => preview(img));
-	btn.addEventListener("keyup", (e) => e.key === "Enter" && preview(img));
-	btn.appendChild(img.cloneNode(true));
-	img.replaceWith(btn);
+	img.tabIndex = 0;
+	img.style.cursor = "zoom-in";
+	img.ariaDescription = "Press Enter or click on image to preview";
+	img.addEventListener("click", (e) => preview(img));
+	img.addEventListener("keyup", (e) => e.key === "Enter" && preview(img));
 });
 
 function preview(img) {
 	lbx.innerHTML = `<img src="${img.src}" alt="${img.alt}"
 	                      srcset="${img.srcset}" sizes="100vw"
-	                      referrerpolicy="${img.referrerPolicy}">
-	                 <p autofocus>Press <kbd>Escape</kbd> or click anywhere to close</p>`;
+	                      referrerpolicy="${img.referrerPolicy}">`;
 	lbx.showModal();
 }
